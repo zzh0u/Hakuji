02c377621cd8b1078fd13595e0bdd34067b6566f
zzh0u <weirong.zhou@outlook.com>
2024-08-16 12:11:20 +0800

    修改数据库

--- database/database.go
+++ database/database.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 
 	// _ 是为了让编译器导入包，但不将其作为当前文件的导出成员。
@@ -10,7 +9,7 @@ import (
 )
 
 func main() {
-	// 配置数据库连接字符串
+	// 配置数据库连接字符串, dsn:Data Source Name
 	dsn := "zhou:030507@/BlockChain"
 
 	// 打开数据库连接
@@ -26,28 +25,28 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// 执行查询
-	rows, err := db.Query("SELECT * FROM your_table_name")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
+	// // 执行查询
+	// rows, err := db.Query("SELECT * FROM your_table_name")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer rows.Close()
 
-	// 遍历结果
-	for rows.Next() {
-		// 处理每一行数据
-		// 这里需要根据你的表结构来定义变量
-		var id int
-		var name string
-		err = rows.Scan(&id, &name)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("ID: %d, Name: %s\n", id, name)
-	}
+	// // 遍历结果
+	// for rows.Next() {
+	// 	// 处理每一行数据
+	// 	// 这里需要根据你的表结构来定义变量
+	// 	var id int
+	// 	var name string
+	// 	err = rows.Scan(&id, &name)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	fmt.Printf("ID: %d, Name: %s\n", id, name)
+	// }
 
-	// 检查是否有错误发生
-	if err = rows.Err(); err != nil {
-		log.Fatal(err)
-	}
+	// // 检查是否有错误发生
+	// if err = rows.Err(); err != nil {
+	// 	log.Fatal(err)
+	// }
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,17 @@ module blockchain
 go 1.22.5
 
 require (
-	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/go-sql-driver/mysql v1.8.1
 	github.com/gorilla/mux v1.8.1
+)
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	golang.org/x/text v0.17.0 // indirect
+	gorm.io/driver/sqlite v1.5.6 // indirect
+	gorm.io/gorm v1.25.11 // indirect
 )
